c63564c175f35742a05d28b2d85c86f16b003505
Naoya Ueda <uedanaoya1174@gmail.com>
2021-10-08 18:04:26 +0900

    1640. Check Array Formation Through Concatenation

--- 1640. Check Array Formation Through Concatenation/solution.rb
+++ 1640. Check Array Formation Through Concatenation/solution.rb
@@ -1,1 +1,10 @@
 # https://leetcode.com/problems/check-array-formation-through-concatenation/
+
+# @param {Integer[]} arr
+# @param {Integer[][]} pieces
+# @return {Boolean}
+def can_form_array(arr, pieces)
+  arr_orderd = arr.map.with_index.to_a.to_h
+  pieces_ordered = pieces.sort_by{|p| arr_orderd[p.first] || -1}
+  arr == pieces_ordered.flatten
+end
