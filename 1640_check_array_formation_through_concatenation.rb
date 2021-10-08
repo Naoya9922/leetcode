2bea5585c4d270c13aee08120d120266a7a996c6
Naoya Ueda <uedanaoya1174@gmail.com>
2021-10-08 20:50:32 +0900

    1640. Check Array Formation Through Concatenation

--- 1640_check_array_formation_through_concatenation.rb
+++ 1640_check_array_formation_through_concatenation.rb
@@ -4,7 +4,7 @@
 # @param {Integer[][]} pieces
 # @return {Boolean}
 def can_form_array(arr, pieces)
-  arr_orderd = arr.map.with_index.to_a.to_h
-  pieces_ordered = pieces.sort_by{|p| arr_orderd[p.first] || -1}
+  arr_ordered = arr.map.with_index.to_a.to_h
+  pieces_ordered = pieces.sort_by{|p| arr_ordered[p.first] || -1}
   arr == pieces_ordered.flatten
 end
