5b5bf69706365b6f257aed62409af8fab6564b81
Naoya Ueda <uedanaoya1174@gmail.com>
2021-10-09 10:54:28 +0900

    1556. Thousand Separator

---
+++ 1556 _thousand_separator.rb
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/thousand-separator/submissions/
+
+# @param {Integer} n
+# @return {String}
+def thousand_separator(n)
+  n.to_s.chars.reverse.join.split(/(\d{3})/).reject(&:empty?).join('.').reverse
+end
