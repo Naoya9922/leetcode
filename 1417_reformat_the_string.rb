076262c78809bf4e9ab13be2f74cdda7f95beefe
Naoya Ueda <uedanaoya1174@gmail.com>
2021-10-09 13:00:13 +0900

    1417. Reformat The String

---
+++ 1417_reformat_the_string.rb
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/reformat-the-string/submissions/
+
+# @param {String} s
+# @return {String}
+def reformat(s)
+  alpha = s.scan(/[a-z]/)
+  numeric = s.scan(/\d/)
+  return "" if (alpha.length - numeric.length).abs >= 2
+  if alpha.length > numeric.length
+    numeric << ""
+    alpha.zip(numeric).join
+  else
+    alpha << ""
+    numeric.zip(alpha).join
+  end
+end
