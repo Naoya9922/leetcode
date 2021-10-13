82c5cea4ad6162c32ad3266322d1de9e8b4309b7
Naoya Ueda <uedanaoya1174@gmail.com>
2021-10-13 10:09:07 +0900

    0185. Department Top Three Salaries

---
+++ 0185_department_top_three_salaries.sql
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/department-top-three-salaries/
+
+# Write your MySQL query statement below
+SELECT
+  Department,
+  Employee,
+  Salary
+FROM
+  (
+  SELECT
+    D.Name AS Department,
+    E.Name AS Employee,
+    E.Salary,
+    DENSE_RANK() OVER(PARTITION BY DepartmentId ORDER BY E.Salary DESC) AS RANKING
+  FROM Employee E
+    INNER JOIN Department D
+    ON E.DepartmentId = D.Id
+) tmp
+WHERE RANKING <= 3
+;
