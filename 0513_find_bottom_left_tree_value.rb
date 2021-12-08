1ab80ed5cbc67c40a299fc8075637847a8aec7a2
Naoya Ueda <uedanaoya1174@gmail.com>
2021-12-08 15:25:54 +0900

    0513. Find Bottom Left Tree Value

---
+++ 0513_find_bottom_left_tree_value.rb
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/find-bottom-left-tree-value/
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer}
+def find_bottom_left_value(root)
+  queue = [root]
+  (0..).each do
+    new_queue = []
+    queue.each do |e|
+      new_queue << e.left if e.left
+      new_queue << e.right if e.right
+    end
+    break if new_queue.empty?
+    queue = new_queue
+  end
+  queue.first.val
+end
