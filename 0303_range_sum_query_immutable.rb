fbcfd4840390f401eddfb5748807ceb7966ce4b6
Naoya9922 <uedanaoya1174@gmail.com>
2022-01-17 09:32:47 +0900

    0303. Range Sum Query - Immutable

---
+++ 0303_range_sum_query_immutable.rb
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/range-sum-query-immutable/
+
+class NumArray
+  attr_accessor :nums
+=begin
+  :type nums: Integer[]
+=end
+  def initialize(nums)
+    @nums = nums
+  end
+
+=begin
+  :type left: Integer
+  :type right: Integer
+  :rtype: Integer
+=end
+  def sum_range(left, right)
+    nums[left..right].sum
+  end
+end
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray.new(nums)
+# param_1 = obj.sum_range(left, right)
