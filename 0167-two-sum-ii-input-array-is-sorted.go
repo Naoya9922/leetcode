8e1970efcb272ed85fe99e607382e10ce84fb96c
Naoya9922 <uedanaoya1174@gmail.com>
2022-01-31 10:35:04 +0900

    0167. Two Sum II - Input Array Is Sorted

---
+++ 0167-two-sum-ii-input-array-is-sorted.go
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+func twoSum(numbers []int, target int) []int {
+	for i := 0; i < len(numbers); i++ {
+		for j := i + 1; j < len(numbers); j++ {
+			if numbers[i]+numbers[j] == target {
+				return []int{i + 1, j + 1}
+			}
+		}
+	}
+	return []int{}
+}
