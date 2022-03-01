b366f6a5abee2a46c5850b1d3b2112e9620b20db
Naoya9922 <uedanaoya1174@gmail.com>
2022-03-01 23:01:03 +0900

    0535. Encode and Decode TinyURL

---
+++ 0535_encode_and_decode_tinyurl.rb
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/encode-and-decode-tinyurl/
+
+require 'base64'
+
+# Encodes a URL to a shortened URL.
+#
+# @param {string} longUrl
+# @return {string}
+def encode(longUrl)
+  Base64.encode64(longUrl)
+end
+
+# Decodes a shortened URL to its original URL.
+#
+# @param {string} shortUrl
+# @return {string}
+def decode(shortUrl)
+  Base64.decode64(shortUrl)
+end
+
+# Your functions will be called as such:
+# decode(encode(url))
