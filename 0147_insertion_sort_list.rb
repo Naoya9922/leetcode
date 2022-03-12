5706e8a973040efe16d88202c1ac0e460c7d7b62
Naoya9922 <uedanaoya1174@gmail.com>
2022-03-12 20:54:42 +0900

    0147. Insertion Sort List

---
+++ 0147_insertion_sort_list.rb
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/insertion-sort-list/
+
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @return {ListNode}
+def insertion_sort_list(head)
+  ary = []
+  while(head)
+    ary << head.val
+    head = head.next
+  end
+  ary.sort
+end
