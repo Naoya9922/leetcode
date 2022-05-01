bdf291d5e82ce2e9dfaecab6e301a7105980451f
Naoya9922 <uedanaoya1174@gmail.com>
2022-05-02 08:11:17 +0900

    2215. Find the Difference of Two Arrays

---
+++ 2215_find_the_difference_of_two_arrays.rb
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/find-the-difference-of-two-arrays/
+
+# @param {Integer[]} nums1
+# @param {Integer[]} nums2
+# @return {Integer[][]}
+def find_difference(nums1, nums2)
+  [(nums1 - nums2).uniq, (nums2 - nums1).uniq]
+end
