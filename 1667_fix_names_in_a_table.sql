b1ef04239d721f704e8874c5a02be88589c5f44f
Naoya Ueda <uedanaoya1174@gmail.com>
2022-09-24 15:05:27 +0900

    1667. Fix Names in a Table

---
+++ 1667_fix_names_in_a_table.sql
@@ -0,0 +1,9 @@
+-- https://leetcode.com/problems/fix-names-in-a-table/
+
+-- Write your MySQL query statement below
+SELECT
+  user_id,
+  CONCAT(UPPER(SUBSTRING(name, 1, 1)), LOWER(SUBSTRING(name, 2))) name
+FROM Users
+ORDER BY user_id
+;
