ab6323908a771effe08a760da54b6425d4217eb8
Naoya Ueda <uedanaoya1174@gmail.com>
2022-10-17 09:05:39 +0900

    0027. Remove Element

---
+++ 0027-remove-element.go
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/remove-element/
+
+func removeElement(nums []int, val int) int {
+	pos := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[pos] = nums[i]
+			pos++
+		}
+	}
+	return pos
+}
